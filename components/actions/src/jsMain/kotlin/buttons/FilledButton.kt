b2cbae7e6b3a9016fac99663beb70c4474c476e9
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2023-10-08 14:44:31 +0200

    feat(actions): Create FilledButton

---
+++ components/actions/src/jsMain/kotlin/buttons/FilledButton.kt
@@ -0,0 +1,37 @@
+package opensavvy.material3.tailwind.actions.buttons
+
+import androidx.compose.runtime.Composable
+import opensavvy.material3.tailwind.ExperimentalComponent
+import opensavvy.material3.tailwind.UnfinishedComponent
+import org.jetbrains.compose.web.attributes.AttrsScope
+import org.w3c.dom.HTMLButtonElement
+
+/**
+ * A button that displays a [label], and performs an [action] when the user clicks it.
+ *
+ * ### Usage
+ *
+ * Filled buttons have the most visual impact after the FAB, and should be used for important, final actions
+ * that complete a flow, like "Save", "Join now" or "Confirm".
+ *
+ * ### External resources
+ *
+ * [Material3 Specs](https://m3.material.io/components/buttons/overview).
+ *
+ * @param label Text description of the action that will occur if the user clicks on it.
+ * Capitalize the first word and proper nouns to allow distinguishing proper nouns.
+ * @param icon Optional icon to help draw attention to the button.
+ */
+@ExperimentalComponent
+@UnfinishedComponent
+@Composable
+fun FilledButton(
+	label: String,
+	action: () -> Unit,
+	enabled: Boolean = true,
+	icon: (@Composable () -> Unit)? = null,
+	attrs: AttrsScope<HTMLButtonElement>.() -> Unit = {},
+) {
+	// For now, it's just an alias for a text button
+	TextButton(label, action, enabled, icon, attrs)
+}
--- demo/src/jsMain/kotlin/components/actions/Buttons.kt
+++ demo/src/jsMain/kotlin/components/actions/Buttons.kt
@@ -3,12 +3,18 @@ package opensavvy.material3.tailwind.demo.components.actions
 import androidx.compose.runtime.Composable
 import kotlinx.browser.window
 import opensavvy.material3.tailwind.actions.buttons.ElevatedButton
+import opensavvy.material3.tailwind.actions.buttons.FilledButton
 import opensavvy.material3.tailwind.actions.buttons.TextButton
 
 @Composable
 fun Buttons() {
 	ElevatedButton(
 		"Elevated button",
+		action = { window.alert("Clicked") },
+	)
+
+	FilledButton(
+		"Filled button",
 		action = { window.alert("Clicked") },
 	)
 
