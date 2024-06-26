3843dbc3767c0d0d8c390c53c805a097d230aa97
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2024-06-26 17:52:36 +0200

    feat(components): Minimal styling for TextField
    See #77

--- components/src/jsMain/kotlin/inputs/TextField.kt
+++ components/src/jsMain/kotlin/inputs/TextField.kt
@@ -3,6 +3,7 @@ package opensavvy.material3.tailwind.inputs
 import androidx.compose.runtime.Composable
 import opensavvy.material3.tailwind.ExperimentalComponent
 import opensavvy.material3.tailwind.UnfinishedComponent
+import org.jetbrains.compose.web.attributes.disabled
 import org.jetbrains.compose.web.dom.Div
 import org.jetbrains.compose.web.dom.Label
 import org.jetbrains.compose.web.dom.Text
@@ -27,37 +28,44 @@ fun TextField(
 	label: String,
 	text: String?,
 	onChange: (String?) -> Unit,
+	enabled: Boolean = true,
 	supportingText: String? = null,
 	errorText: String? = null,
 	style: FieldStyle = FieldStyle.Outlined,
 	leadingIcon: (@Composable () -> Unit)? = null,
 	trailingIcon: (@Composable () -> Unit)? = null,
-) {
+) = Div {
 	Label(attrs = {
 		classes(
 			"mdk-field",
-			if (style == FieldStyle.Outlined) "mdk-field-outlined" else "mdk-field-filled"
+			if (style == FieldStyle.Outlined) "mdk-field-outlined" else "mdk-field-filled",
 		)
+
+		if (!enabled)
+			classes("mdk-field-disabled")
 	}) {
 		Text(label)
 
 		Div({
-			classes("flex", "gap-2")
+			classes("mdk-field-content")
 		}) {
 			leadingIcon?.invoke()
 
 			TextInput(text ?: "") {
 				this.onInput { onChange(it.value.takeIf(String::isNotBlank)) }
 
-				classes("outline-none")
+				classes("mdk-field-inner-content")
+
+				if (!enabled)
+					disabled()
 			}
 
 			trailingIcon?.invoke()
 		}
-
-		if (errorText != null)
-			Text(errorText)
-		else if (supportingText != null)
-			Text(supportingText)
 	}
+
+	if (errorText != null)
+		Text(errorText)
+	else if (supportingText != null)
+		Text(supportingText)
 }
